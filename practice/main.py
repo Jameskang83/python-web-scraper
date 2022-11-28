1a1aa90163ac3f4e62e9989f007f77fdc99d4a29
jimkang83 <jim.kang83@gmail.com>
2022-11-27 22:40:26 -0700

    0.1 Created practice

---
+++ practice/main.py
@@ -0,0 +1,38 @@
+# print("hello")
+# a = 2
+# b = 3
+# c = a+b
+# print(c)
+
+# my_name = "jim"
+# print(my_name)
+
+# dead = True
+# print(dead)
+
+# def say_hello(user_name, user_age):
+#   print("hello", user_name, user_age)
+
+# say_hello("jim", 40)
+
+# def say_hello(user_name="you"):
+#   print("hello", user_name)
+
+# say_hello("jim")
+# say_hello()
+
+# def tax_calc(money):
+#   return money * 0.35
+
+# def pay_tax(tax):
+#   print("thanks", tax)
+
+# to_pay = tax_calc(1500)
+# pay_tax(to_pay)
+
+my_name = "jim"
+my_age = 12
+my_color_eyes = "brown"
+
+print(
+  f"Hello, {my_name}, i have {my_age} years, {my_color_eyes} is my eye color ")
